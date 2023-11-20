0b5a0c9c0a4cae5959ca076d8f65bfa51831b3ac
xonx4l <sk797119@gmail.com>
2023-11-20 23:43:51 +0530

    update code.go

--- compiler /code.go
+++ compiler /code.go
@@ -1,5 +1,31 @@
-package compiler 
+import (
+	"encoding/binary"
+	"fmt"
+)
 
-import {
-	    "monkey/"
+func Make(op Opcode, operands ...int) []byte {
+	 def, ok := definitions[op]
+	 if !ok {
+		 return []byte{}
+	 }
+
+	 instructionLen := 1
+	 for _, w := range def.OperandsWidths {
+		instructionLen += w	
+	 }
+
+	 instruction := make[]byte, instructionLen)
+	 instruction[0] = byte(op)
+
+	 offset :=1
+	 for i,o := range operands{
+		width := def.OperandsWidth[i]
+		switch width {
+		case 2:
+			binary.BigEndian.PutUint16[offset:], uint16(o))
+		}
+		offset += width
+	 }
+
+	 return instruction
 }
